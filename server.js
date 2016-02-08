5eecb1745172fd3b73916045b145c6e747fba7a7
Ryan Schneider <schneider.620@osu.edu>
2016-02-07 21:43:40 -0500

    Output port info

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.get('/wines/:id', function(req, res) {
 });
  
 app.listen(process.env.PORT || 3000);
-console.log('Listening on port 3000...');
+console.log('Listening on port  ' + listener.address().port + "...");
